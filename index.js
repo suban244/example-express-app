82dba7e9c7b1eae46749eab6ea939937a0b500d6
Suban <subanshrestha244@gmail.com>
2025-05-06 22:12:18 +0545

    add render deploy hook

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h2>Hello World4!</h2>')
+  res.send('<h2>Hello World5!</h2>')
 })
 
 const PORT = 10000
